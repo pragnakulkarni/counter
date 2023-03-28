ae02b9a617dce0310b943eea030b70b4eb863958
pragnakulkarni <70208892+pragnakulkarni@users.noreply.github.com>
2023-03-28 07:51:29 +0530

    Add files via upload

---
+++ js_counter.js
@@ -0,0 +1,20 @@
+function add()
+{
+
+    var n = Number(document.getElementById('start').innerHTML)
+    document.getElementById('start').innerHTML=n+1
+
+}
+function sub()
+{
+
+    var n = Number(document.getElementById('start').innerHTML)
+    document.getElementById('start').innerHTML=n-1
+
+}
+function zero()
+{
+
+    document.getElementById('start').innerHTML=0
+
+}
